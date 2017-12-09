328262e04e98e24131b970e5b38c28889ce3bea4
Simon <simon@worthyplatoon.com>
2017-12-09 17:37:51 -0500

    Base2

---
+++ SiBot/SiBot/Modules/Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+
+
+namespace SiBot.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task testCmd()
+        {
+            await Context.Channel.SendMessageAsync("Test Successfull!");
+        }
+
+        private bool _galnetEnabled = false;
+        private string[] _allowed = { "on", "off" };
+
+        [Command("GalNet")]
+        public async Task galNet0(params string[] _allowed)
+        {
+            if (_allowed == off)
+            {
+                if (_galnetEnabled == false)
+                {
+                    await Context.Channel.SendMessageAsync("GalNet Alerts Are Already OFF");
+                }
+                else
+                {
+                    _galnetEnabled = false;
+                    await Context.Channel.SendMessageAsync("GalNet Alerts Are Now OFF");
+                }
+            }
+            if (_allowed == on)
+            {
+                if (_galnetEnabled == false)
+                {
+                    await Context.Channel.SendMessageAsync("GalNet Alerts Are Already ON");
+                }
+                else
+                {
+                    _galnetEnabled = false;
+                    await Context.Channel.SendMessageAsync("GalNet Alerts Are Now ON");
+                }
+            }
+        }
+    }
+}
+
--- SiBot/SiBot/Program.cs
+++ SiBot/SiBot/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
 
 namespace SiBot
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
         {
+            _client = new DiscordSocketClient();
+
+            new CommandHandler();
+
+            await _client.LoginAsync(TokenType.Bot, "Mzg4OTk2NjczNzU5ODcwOTc3.DQ1K6g.lLVSiy6szldBXkn01cCiu2qxYcc");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler();
+
+            await _handler.InitializeAsync(_client);
+
+            await Task.Delay(-1);
         }
+
     }
 }
+
+
+
+
